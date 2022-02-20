853fb921bc8742f1e6f7def5c31537b4fd5c2d85
janusha <janushas@dsny.com>
2022-02-20 18:32:02 -0500

    active link using NavLink

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
