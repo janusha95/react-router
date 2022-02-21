9fcaad5d327bb8637e3894f809e97e5c27c05f03
janusha <janushas@dsny.com>
2022-02-20 19:08:51 -0500

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
 import NoMatch from "./components/NoMatch";
+import Products from "./components/Products";
+import NewProducts from "./components/NewProducts";
+import FeaturedProducts from "./components/FeaturedProducts";
 
 function App() {
   return (
@@ -15,6 +18,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
