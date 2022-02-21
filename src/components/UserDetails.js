3b427df4acd9198b3103b46b52ce1745d37baa84
janusha <janushas@dsny.com>
2022-02-20 19:24:40 -0500

    URL params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function UserDetails() {
-  return <div>UserDetails</div>;
+  // you can either use this
+  //   const params = useParams();
+  //   const userId = params.userId;
+  // Or you can destructure the params object and use like this:
+  const { userId } = useParams();
+
+  return <div>UserDetails of user {userId} </div>;
 }
 
 export default UserDetails;
